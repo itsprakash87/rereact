20e0a4663c353cd25d040ab93efe373ef03407ca
prakash <itsprakash87@gmail.com>
2018-05-10 02:14:26 +0530

    Added logic for unmount component

--- unmountComponents.js
+++ unmountComponents.js
@@ -1,4 +1,35 @@
 
-export function unmountComponents() {
-    
+let deepnessLevel = 0;
+let unmountQueue = [];
+
+export function unmountComponents(dom) {
+    deepnessLevel++;
+
+    if (dom) {
+        let childNodes = dom.childNodes || [];
+
+        childNodes.map(function(node) {
+            unmountComponents()
+        });
+
+        if (dom && dom._componentInstance) {
+            unmountQueue.push(dom._componentInstance);
+        }
+    }
+
+    deepnessLevel--;
+
+    if (deepnessLevel === 0) {
+        if (dom && dom.parentNode) {
+            dom.parentNode.removeChild(dom);
+        }
+        flushUnmountQueue();
+    }
 }
+
+export function flushUnmountQueue() {
+    for(let i in unmountQueue) {
+        unmountQueue[i] && unmountQueue[i].componentWillUnmount && unmountQueue[i].componentWillUnmount();
+    }
+}
+
