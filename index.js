8b2d0eb2802ecf046142e86332b5d16302bf43d0
prakash <itsprakash87@gmail.com>
2018-05-20 16:03:02 +0530

    Exported react api methods

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 import { createElement } from './src/createElement';
 import { Component } from './src/component';
 import { render } from './src/render';
+import { cloneElement, findDOMNode } from './src/rereactApi';
 
 export default {
 	createElement,
 	Component,
 	render,
+	cloneElement,
+	findDOMNode
 };
 
 export {
 	createElement,
 	Component,
 	render,
+	cloneElement,
+	findDOMNode
 };
 
